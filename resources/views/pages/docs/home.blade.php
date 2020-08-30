55e7c62af663bff118b136b0625671b619c3687f
luaemu <luaemuinquiries@gmail.com>
2020-08-29 19:07:30 -0500

    stuff

--- resources/views/pages/docs/home.blade.php
+++ resources/views/pages/docs/home.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     <div class="jumbotron hide-bg h-100">
         <h1 class="display-3">Under Construction</h1>
+        <hr class="ys-3">
         <p class="lead">Documentation coming soon...</p>
     </div>
 @endsection
